b0e43b42542f553e97dbba664d3d786eb4d2d193
BuddMcCormack <cmccormack1437@gmail.com>
2024-12-06 20:03:45 -0500

    Added prefabs, changed materials

--- Assets/SpawnObject.cs
+++ Assets/SpawnObject.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SpawnObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject CannonBall;
+    public GameObject TennisBall;
+    public GameObject DodgeBall;
+    public GameObject MetalBall;
+    public GameObject GlassBall;
+    public int CannonBallCount = 5;
+    public int TennisBallCount = 10;
+    public int DodgeBallCount = 15;
+    public int MetalBallCount = 8;
+    public int GlassBallCount = 8;
+    public float Movespeed = 8f;
+    bool canMove = true;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (canMove == true)
+        {
+
+            Vector3 moveOffset = Vector3.zero;
+            moveOffset.x = Input.GetAxis("Horizontal");
+            moveOffset.x = moveOffset.x * Movespeed * Time.deltaTime;
+            transform.position += moveOffset;
+        }
+
     }
 }
